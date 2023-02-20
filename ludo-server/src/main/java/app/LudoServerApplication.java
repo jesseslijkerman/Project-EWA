6ea7ca101bbd0d10ee0d8e14a11f49a19e8c84c6
Jesse Slijkerman <jesse.slijkerman2@hva.nl>
2023-02-20 11:38:26 +0100

    Setup backend

---
+++ ludo-server/src/main/java/app/LudoServerApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LudoServerApplication.class, args);
+    }
+
+}
---
+++ ludo-server/src/test/java/hva/nl/ludoserver/LudoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hva.nl.ludoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
