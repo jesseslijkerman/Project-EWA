b8e918c2f58923ef65180e8f8c68df9c2063ea6a
Jesse Slijkerman <jesse.slijkerman2@hva.nl>
2023-03-13 19:50:54 +0100

    Fixes

--- ludo-server/src/main/java/app/LudoServerApplication.java
+++ ludo-server/src/main/java/app/LudoServerApplication.java
@@ -1,9 +1,12 @@
 package app;
 
+import app.controllers.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = UserController.class)
 public class LudoServerApplication {
 
     public static void main(String[] args) {
--- ludo-server/src/main/java/app/models/UserLobby.java
+++ ludo-server/src/main/java/app/models/UserLobby.java
@@ -1,12 +1,11 @@
 package app.models;
 
+import app.compositeKeys.UserLobbyPK;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 @Entity
+@IdClass(UserLobbyPK.class)
 public class UserLobby {
     @Id
     @ManyToOne
@@ -14,6 +13,7 @@ public class UserLobby {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @Id
     @ManyToOne
     @JsonBackReference
     @JoinColumn(name = "lobby_id")
@@ -27,5 +27,54 @@ public class UserLobby {
 
     private int pawnPosition4;
 
+    public UserLobby() {
+    }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Lobby getLobby() {
+        return lobby;
+    }
+
+    public void setLobby(Lobby lobby) {
+        this.lobby = lobby;
+    }
+
+    public int getPawnPosition1() {
+        return pawnPosition1;
+    }
+
+    public void setPawnPosition1(int pawnPosition1) {
+        this.pawnPosition1 = pawnPosition1;
+    }
+
+    public int getPawnPosition2() {
+        return pawnPosition2;
+    }
+
+    public void setPawnPosition2(int pawnPosition2) {
+        this.pawnPosition2 = pawnPosition2;
+    }
+
+    public int getPawnPosition3() {
+        return pawnPosition3;
+    }
+
+    public void setPawnPosition3(int pawnPosition3) {
+        this.pawnPosition3 = pawnPosition3;
+    }
+
+    public int getPawnPosition4() {
+        return pawnPosition4;
+    }
+
+    public void setPawnPosition4(int pawnPosition4) {
+        this.pawnPosition4 = pawnPosition4;
+    }
 }
