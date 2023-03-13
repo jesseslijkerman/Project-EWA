6f1955d216e99a1a81f391c2f8c5318e36ed1c2c
Jesse Slijkerman <jesse.slijkerman2@hva.nl>
2023-03-13 17:43:47 +0100

    Added lobby model

---
+++ ludo-server/src/main/java/app/models/Lobby.java
@@ -0,0 +1,77 @@
+package app.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Lobby {
+    @Id
+    private Long id;
+    private int maxPlayers;
+    private int turnTimer;
+    private int gameMode;
+    private String name;
+    private String password;
+    @CreationTimestamp
+    private LocalDateTime created;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public void setMaxPlayers(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+
+    public int getTurnTimer() {
+        return turnTimer;
+    }
+
+    public void setTurnTimer(int turnTimer) {
+        this.turnTimer = turnTimer;
+    }
+
+    public int getGameMode() {
+        return gameMode;
+    }
+
+    public void setGameMode(int gameMode) {
+        this.gameMode = gameMode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+}
