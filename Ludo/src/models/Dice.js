5069c7547ae3f340694981e3909be5e3248fa408
Tristan Scholtens <tristan.scholtens@gmail.com>
2023-03-14 16:01:57 +0100

    opgeschoond

--- Ludo/src/models/Dice.js
+++ Ludo/src/models/Dice.js
@@ -8,19 +8,11 @@ export class Dice {
 
     static rollDice(){
     let random = Math.floor(Math.random() * 6 + 1);
-
-        if (random == 6){
-            return random
-        } else {
-            return random
-        }
-
+    return random
     }
 
     static createDiceRoll(){
-
         const eyes = this.rollDice();
-
         return new Dice(eyes)
     }
 
