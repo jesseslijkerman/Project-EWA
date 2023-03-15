dbdd7193283e6d8f9ea4cfff79f22338b95eee4d
Tristan Scholtens <tristan.scholtens@gmail.com>
2023-03-15 14:28:30 +0100

    show en hide van last roll is nu specifiek voor gekozen speler

--- Ludo/src/components/DiceRoll.vue
+++ Ludo/src/components/DiceRoll.vue
@@ -1,23 +1,19 @@
 
 
 <template>
-  <div class="player" v-for="players in playerList" :key="players">
+  <div class="player" v-for="(players, index) in playerList" :key="players">
     <span :style="[currentPlayer == players.id ? { 'background': players.name  }: players.style ]" class="dot"></span>
     <img class=dice @click="rollDice" :src="getRollPicture(players.currentRoll.eyes)">
 
 
     <br>
 
-    <div v-if="!hide" v-on:click="hide = !hide">
-      <p>Click to show last roll ▼</p>
+    <div @click="toggleItem(index)">
+      <p @click="toggleCondition"> {{ showText }}</p>
     </div>
 
-    <div v-if="hide" v-on:click="hide = !hide">
-      <p>Click to hide last roll ▲</p>
-    </div>
-
-    <div v-if="hide">
-      <img class=priorDice :src="getRollPicture(players.priorRoll.eyes)">
+    <div v-if="activeIndices.includes(index)">
+      <img  class=priorDice :src="getRollPicture(players.priorRoll.eyes)">
     </div>
 
     <button v-if="currentPlayer == players.id" :disabled="disabled" @click="rollDice">Roll dice</button>
@@ -35,7 +31,7 @@ export default {
   data(){
     return{
       disabled: false,
-      hide: false,
+      hideAndShowText: true,
       playerList: [
           { id: 0, name: "red", currentRoll:0, priorRoll: 0, style: {'border-color': "red"}},
           { id: 1, name: "blue", currentRoll:0, priorRoll: 0, style: {'border-color':"blue" }},
@@ -43,7 +39,7 @@ export default {
           { id: 3, name: "yellow", currentRoll:0, priorRoll: 0,style: {'border-color':"yellow" }}
       ],
       currentPlayer: 0,
-      bing: null
+      activeIndices: []
     }
   },
 
@@ -69,7 +65,32 @@ export default {
         return "src/assets/dice" + src + ".png"
       }
     },
+
+    toggleItem(index) {
+      if (this.activeIndices.includes(index)) {
+        // If the item is already active, remove it from the activeIndices array
+        this.activeIndices.splice(this.activeIndices.indexOf(index), 1)
+      } else {
+        // If the item is not active, add it to the activeIndices array
+        this.activeIndices.push(index)
+      }
+    },
+
+    toggleCondition(){
+      this.hideAndShowText = !this.hideAndShowText
+    }
+
   },
+
+
+  computed :{
+    showText(){
+      return this.hideAndShowText ? 'Click to show last roll ▼' : 'Click to hide last roll ▼'
+    }
+  }
+
+
+
 }
 
 </script>
