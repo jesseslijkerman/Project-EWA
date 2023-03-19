241e0c3ee8d0da78224ea7155fe7159835ca60f7
Robbie <rob.loeffen@hva.nl>
2023-03-20 00:28:20 +0100

    first draft login with test data from localstorage. need to fix the styling mostly now

--- Ludo/src/router/index.js
+++ Ludo/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import Login from "@/components/Login.vue";
+import Register from "@/components/Register.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +18,22 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+      meta: {
+        hideNavbar: true,
+      }
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register,
+      meta: {
+        hideNavbar: true,
+      }
     },
   ],
 });
