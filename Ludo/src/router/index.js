c2622577f1ba250b9de8b8c77e296c1761772e9d
Calvin <calvin.nessen@outlook.com>
2023-03-20 09:46:19 +0100

    fixed router

--- Ludo/src/router/index.js
+++ Ludo/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Login from "@/components/Login.vue";
 import Register from "@/components/Register.vue";
+import LudoBoard from "../components/LudoBoard";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/game",
+      name: "game",
+      component: LudoBoard
     },
     {
       path: "/login",
