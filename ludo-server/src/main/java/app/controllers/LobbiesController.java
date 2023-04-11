29524a04ccbef1662be34c969fb4654c64e5dc52
Jesse Slijkerman <jesse.slijkerman2@hva.nl>
2023-04-11 21:15:09 +0200

    Added backend logic for Lobby

---
+++ ludo-server/src/main/java/app/controllers/LobbiesController.java
@@ -0,0 +1,62 @@
+package app.controllers;
+
+import app.exceptions.PreConditionFailed;
+import app.exceptions.ResourceNotFound;
+import app.models.Lobby;
+import app.repositories.LobbiesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/lobbies")
+public class LobbiesController {
+    @Autowired
+    private LobbiesRepository lobbyRepo;
+
+    @GetMapping(path = "", produces = "application/json")
+    public List<Lobby> getAllLobbys(){
+        return this.lobbyRepo.findAll();
+    }
+
+    @GetMapping(path = "/{id}", produces = "application/json")
+    public Lobby findById(@PathVariable int id){
+        Lobby lobby = this.lobbyRepo.findById(id);
+        if (lobby==null){
+            throw new ResourceNotFound("id-" + id);
+        }
+        return lobby;
+    }
+
+    @PostMapping(path = "")
+    public ResponseEntity<Object> createLobby(@RequestBody Lobby lobby){
+        Lobby createdLobby = lobbyRepo.save(lobby);
+
+        // Return appropriate response status
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdLobby.getId()).toUri();
+        return ResponseEntity.created(location).body(createdLobby);
+    }
+
+    @PutMapping(path = "/{id}")
+    public Lobby editLobby(@PathVariable int id, @RequestBody Lobby lobby){
+        if (lobby.getId() != id){
+            throw new PreConditionFailed("id-" + id + " doesn't match id of body");
+        }
+
+        lobbyRepo.save(lobby);
+        return lobby;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public Lobby deleteLobby(@PathVariable int id){
+        Lobby lobby = lobbyRepo.deleteById(id);
+        if (lobby==null){
+            throw new ResourceNotFound("id-" + id);
+        }
+        return lobby;
+    }
+}
---
+++ ludo-server/src/main/java/app/repositories/LobbiesRepository.java
@@ -0,0 +1,36 @@
+package app.repositories;
+
+import app.models.Lobby;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public class LobbiesRepository {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public List<Lobby> findAll(){
+        TypedQuery<Lobby> namedQuery = entityManager.createNamedQuery("find_all_lobbies", Lobby.class);
+        return namedQuery.getResultList();
+    }
+
+    public Lobby findById(int id){
+        return entityManager.find(Lobby.class, id);
+    }
+
+    public Lobby save(Lobby user){
+        return entityManager.merge(user);
+    }
+
+    public Lobby deleteById(int id){
+        Lobby user = findById(id);
+        entityManager.remove(user);
+        return user;
+    }
+}
