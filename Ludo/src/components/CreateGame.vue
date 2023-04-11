8ee1ec404801d364a693bcdbedb97adb2b088624
Jesse Slijkerman <jesse.slijkerman2@hva.nl>
2023-04-11 20:01:31 +0200

    Added create game form

---
+++ Ludo/src/components/CreateGame.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="handleSumbit">
+      <!-- Name input -->
+      <div class="form-outline mb-4">
+        <label class="form-label">Game Mode</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="gameMode"
+          placeholder="Game Mode"
+        />
+      </div>
+
+      <!-- Username input -->
+      <div class="form-outline mb-4">
+        <label class="form-label">Max Players</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="maxPlayers"
+          placeholder="Max Players"
+        />
+      </div>
+
+      <!-- Email input -->
+      <div class="form-outline mb-4">
+        <label class="form-label">Name</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="name"
+          placeholder="Name"
+        />
+      </div>
+
+      <!-- Password input -->
+      <div class="form-outline mb-4">
+        <label class="form-label">Password</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="password"
+          placeholder="Password (optional)"
+        />
+      </div>
+
+      <!-- ConfirmPassword input -->
+      <div class="form-outline mb-4">
+        <label class="form-label">Turn Timer</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="turnTimer"
+          placeholder="Turn Timer"
+        />
+      </div>
+
+      <!-- Submit button -->
+      <div class="text-center">
+        <button
+          type="button"
+          class="btn btn-primary btn-block mb-4"
+          v-on:click="handleSumbit"
+        >
+          Create game
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateGame",
+  data() {
+    return {
+      gameMode: null,
+      maxPlayers: null,
+      name: null,
+      password: null,
+      turnTimer: null,
+    };
+  },
+};
+</script>
+
+<style scoped>
+*
+</style>
