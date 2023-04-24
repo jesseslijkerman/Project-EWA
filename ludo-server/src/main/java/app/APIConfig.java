16c13b7650d6e775ae975fb9f68830986c1e922c
Jesse Slijkerman <jesse.slijkerman2@hva.nl>
2023-04-24 12:20:41 +0200

    Created AuthenticationController

--- ludo-server/src/main/java/app/APIConfig.java
+++ ludo-server/src/main/java/app/APIConfig.java
@@ -9,7 +9,7 @@ public class APIConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*", "http://*.hva.nl:*")
+                .allowedOriginPatterns("http://localhost:*", "http://*.hva.nl:*", " http://127.0.0.1:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
