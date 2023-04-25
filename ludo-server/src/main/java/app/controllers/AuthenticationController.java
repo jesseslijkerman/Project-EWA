fcb5ed16accc7575ec3c83629f07c33aa88b9c7d
Jesse Slijkerman <jesse.slijkerman2@hva.nl>
2023-04-25 20:23:52 +0200

    Temporarily commented out JWTRequestFilter because it broke the backend

--- ludo-server/src/main/java/app/controllers/AuthenticationController.java
+++ ludo-server/src/main/java/app/controllers/AuthenticationController.java
@@ -20,29 +20,29 @@ public class AuthenticationController {
     @Autowired
     APIConfig apiConfig;
 
-    @PostMapping(path = "login")
-    public ResponseEntity<User> logIn(@RequestBody ObjectNode signInInfo){
-        String email = signInInfo.get("email").asText();
-        String password = signInInfo.get("password").asText();
-
-        String[] emailSplit = email.split("@");
-
-        if (Objects.equals(password, emailSplit[0])){
-            User user = new User(1L + (long) (Math.random() * 100L) , emailSplit[0], email, "registered user", password);
-            JWToken jwToken = new JWToken(user.getName(), user.getId(), user.getRole());
-
-            String tokenString = jwToken.encode(
-                    this.apiConfig.getIssuer(),
-                    this.apiConfig.getPassphrase(),
-                    this.apiConfig.getTokenDurationOfValidity()
-            );
-
-            return ResponseEntity.accepted()
-                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenString)
-                    .body(user);
-        } else{
-            throw new NotAcceptable("Cannot authenticate user by email=" + email);
-        }
-
-    }
+//    @PostMapping(path = "login")
+//    public ResponseEntity<User> logIn(@RequestBody ObjectNode signInInfo){
+//        String email = signInInfo.get("email").asText();
+//        String password = signInInfo.get("password").asText();
+//
+//        String[] emailSplit = email.split("@");
+//
+//        if (Objects.equals(password, emailSplit[0])){
+//            User user = new User(1L + (long) (Math.random() * 100L) , emailSplit[0], email, "registered user", password);
+//            JWToken jwToken = new JWToken(user.getName(), user.getId(), user.getRole());
+//
+//            String tokenString = jwToken.encode(
+//                    this.apiConfig.getIssuer(),
+//                    this.apiConfig.getPassphrase(),
+//                    this.apiConfig.getTokenDurationOfValidity()
+//            );
+//
+//            return ResponseEntity.accepted()
+//                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenString)
+//                    .body(user);
+//        } else{
+//            throw new NotAcceptable("Cannot authenticate user by email=" + email);
+//        }
+//
+//    }
 }
