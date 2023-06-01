fd432034376f5d55c88ddd8ef4faba2b994eeb36
Jesse Slijkerman <jesse.slijkerman2@hva.nl>
2023-06-01 12:35:12 +0200

    Fixed login redirect

--- Ludo/src/components/Login.vue
+++ Ludo/src/components/Login.vue
@@ -8,7 +8,7 @@
       <div class="card card-md">
         <div class="card-body">
           <h2 class="h2 text-center mb-4">Login to your account</h2>
-          <form action="./" method="get" autocomplete="off" novalidate="">
+          <form @submit.prevent method="get" autocomplete="off" novalidate="">
             <div class="mb-3"><label class="form-label">Email address</label> <input type="email"
                                                                                      v-model="email"
                                                                                      class="form-control"
@@ -83,7 +83,7 @@ export default {
   methods: {
     async signIn() {
       await this.sessionService.asyncSignIn(this.email, this.password);
-      // location.reload()
+      this.$router.push("/ongoing-matches");
     }
 
   },
