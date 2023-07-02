977e0c7b04e64578eecea18c07ae317f9ba20504
Tristan Scholtens <tristan.scholtens@gmail.com>
2023-07-02 17:09:51 +0200

    fe tests

--- Ludo/tests/unit/TristanTest.spec.js
+++ Ludo/tests/unit/TristanTest.spec.js
@@ -1,9 +1,10 @@
 // Test 1 - Test if Dice model works correctly when thrown
 import { Dice } from '@/models/Dice.js';
 import { shallowMount } from '@vue/test-utils';
-import LudoBoard from "@/components/LudoBoard";
+import LudoBoard from "@/components/LudoBoard.vue";
 
-describe('Dice', () => {
+
+describe('Dice roll eyes testing', () => {
     describe('constructor', () => {
         it('should set the eyes property correctly', () => {
             const dice = new Dice(3);
@@ -30,7 +31,7 @@ describe('Dice', () => {
 });
 
 // Test 2
-describe('getPlayerLocation', () => {
+describe('getPlayerLocation method testing', () => {
     let component;
     const path = [
         { i: 0, j: 0 },
@@ -86,18 +87,99 @@ describe('getPlayerLocation', () => {
 
 // Test 3
 
-import { searchValueInArray } from '@/components/LudoBoard.vue';
+describe('LudoBoard pawn testing', () => {
+    it('should return true if there are pawns on the board', () => {
+        const wrapper = shallowMount(LudoBoard);
 
-// describe('searchValueInArray', () ==> {
-//
-// });
-//
-//
+        wrapper.setData({
+            currentPlayer: 'R',
+            pawns: {
+                'R': [
+                    { startPos: 31, position: -1, home: 0 },
+                    { startPos: 31, position: -1, home: 0 },
+                    { startPos: 31, position: -1, home: 0 },
+                    { startPos: 31, position: -1, home: 0 }
+                ],
+            }
+        });
 
+        // Call the hasPawnsOnBoard method and expect it to return true
+        wrapper.vm.hasPawnsOnBoardMethod();
+        expect(wrapper.vm.hasPawnsOnBoard).toBe(true);
+    });
+
+    it('should return false if there are no pawns on the board', () => {
+        const wrapper = shallowMount(LudoBoard);
+
+        wrapper.setData({
+            pawns: {
+                currentPlayer: 'B',
+                'B': [
+                    { startPos: 31, position: -1, home: 1 },
+                    { startPos: 31, position: -1, home: 1 },
+                    { startPos: 31, position: -1, home: 1 },
+                    { startPos: 31, position: -1, home: 1 }
+                ],
+            }
+        });
+
+        // Call the hasPawnsOnBoard method and expect it to return false
+        wrapper.vm.hasPawnsOnBoardMethod();
+        expect(wrapper.vm.hasPawnsOnBoard).toBe(false);
+    });
+});
 
 // Test 4
 
+import { UserLobbyAdaptor } from '@/services/UserLobbyAdaptor.js';
 
+global.fetch = jest.fn();
+
+describe('UserLobbyAdaptor', () => {
+    let userLobbyAdaptor;
+
+    beforeEach(() => {
+        userLobbyAdaptor = new UserLobbyAdaptor('http://localhost:6969/userLobbies');
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('asyncFindAll should make a GET request with the correct URL', async () => {
+        // mock the fecth response
+        const userLobbies = [{ id: 1, name: 'Lobby 1' }];
+        global.fetch.mockResolvedValueOnce({
+            ok: true,
+            json: () => Promise.resolve(userLobbies),
+        });
+
+        const result = await userLobbyAdaptor.asyncFindAll();
+
+        expect(result).toEqual(userLobbies);
+    });
+
+});
 
 
 // Test 5
+
+describe('LudoBoard won method test', () => {
+    it('should return true if a player has won', () => {
+        const wrapper = shallowMount(LudoBoard);
+        wrapper.setData({ hasWonBool: true });
+
+        wrapper.vm.hasWon();
+        expect(wrapper.vm.hasWonBool).toBe(true);
+    });
+
+    it('should return false if no player has won', () => {
+        const wrapper = shallowMount(LudoBoard);
+        wrapper.setData({ hasWonBool: false });
+
+        wrapper.vm.hasWon();
+        expect(wrapper.vm.hasWonBool).toBe(false);
+    });
+});
+
+
