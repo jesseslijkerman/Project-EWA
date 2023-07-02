4f1548389a447d569629c76b2fe18ba30c1a497a
Martijn <martijn.mulder2@hva.nl>
2023-07-02 13:18:37 +0200

    Auto stash before merge of "feature/Develop" and "origin/feature/Develop"

--- Ludo/tests/unit/MartijnTest.spec.js
+++ Ludo/tests/unit/MartijnTest.spec.js
@@ -6,31 +6,49 @@ jest.mock('@/services/LobbyAdaptor.js')
 jest.mock('@/services/SessionSbService.js')
 jest.mock('@/services/UsersAdaptor.js')
 
-describe('LudoBoard.vue', () => {
-  let wrapper
-
   beforeEach(() => {
-    
-    wrapper = shallowMount(LudoBoard, {
-      propsData: {},
-      mocks: {
-        
-        userLobbyService: {},
-        lobbyService: {},
-        sessionService: {
-          currentAccount: {
-            userName: 'test_user'
-          }
-        },
-        registerService: {},
+    wrapper = shallowMount(WaitingRoom, {
+      data() {
+        return {
+          lobbyData: {
+            status: 'INACTIVE',
+            id: 1,
+            created: '2023-07-02',
+            gameMode: 'Mode',
+            maxPlayers: 4,
+            name: 'Lobby Name',
+            password: 'Password',
+            turnTimer: 60,
+            whoseTurn: 'Player',
+          },
+          isHost: true,
+          playerData: ['Player 1', 'Player 2', 'Player 3', 'Player 4'],
+          lobbyNumber: 1,
+          gameStartedText: 'Match has been started, click here to enter the game!',
+          buttonClicked: false,
+          enoughPlayers: true,
+        };
       },
-      stubs: ['router-link', 'router-view'] 
-    })
-  })
+      mocks: {
+        lobbyService: {
+          asyncFindById: jest.fn().mockResolvedValue('181'),
+          asyncFindUsersInLobby: jest.fn(),
+          asyncRemoveUserFromLobby: jest.fn(),
+          asyncStartMatch: jest.fn(),
+        },
+        sessionService: {
+          currentAccount: { id: 123 },
+        },
+        userLobbyService: {
+          asyncGetPlayerNumber: jest.fn(),
+        },
+      },
+    });
+  });
 
-  it('renders correctly', () => {
-    expect(wrapper.element).toMatchSnapshot()
-  })
+  // afterEach(() => {
+  //     wrapper.destroy();
+  // });
 
   it('creates the path when the component is created', async () => {
     expect(wrapper.vm.path).toBeTruthy() 
@@ -43,3 +61,13 @@ describe('LudoBoard.vue', () => {
 
 
 })
+
+  it('calls start match and sets buttonClicked to true when start match button is clicked', async () => {
+    await wrapper.find('.start-match-button').trigger('click');
+    expect(wrapper.vm.$options.mocks.lobbyService.asyncStartMatch).toBeCalledWith(1);
+    expect(wrapper.vm.buttonClicked).toBe(true);
+  });
+
+  // Add more test cases as needed
+
+});
